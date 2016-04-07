80634a44eb72dcd89214b2a0c6c28bf2ca1c7d11
IhorShmidt <ihshmidt@gmail.com>
2016-04-08 02:14:02 +0300

    Added some Angular

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,3 +1,14 @@
 function meetupsController($scope) {
-   $scope.meetupsCount = 10;
+
+
+   $scope.meetups= [
+      { name: 'MEAN SF Developers'},
+      { name: 'Some other meetup'}
+   ];
+
+   $scope.createMeetup = function() {
+      $scope.meetups.push({ name: $scope.meetupName});
+      $scope.meetupName = '';
+   }
+   $scope.meetupsCount = $scope.meetups.length;
 }
