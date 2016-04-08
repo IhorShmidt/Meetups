d3b7a196a3d998a2232ca5645022b9ed5166d7d5
IhorShmidt <ihshmidt@gmail.com>
2016-04-08 03:12:56 +0300

    Added body-parser, made some changes with out index.html, server.js etc.

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var meetupsController = require('./server/controllers/meetups-controllers');
+
+app.use(bodyParser());
 
 app.get('/', function(req, res) {
-   res.sendfile(__dirname + '/client/views/index.html');
+   res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.use('/js', express.static(__dirname + '/client/js'));
+
+app.post('/api/meetups', meetupsController.create);
 
 app.listen(3000, function() {
   console.log('I`m listening on port 3000');
